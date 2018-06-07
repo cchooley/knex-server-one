68b438fc4a90cc3a7cacec841d82b9adc16da9c2
Conor Hooley <cchooley@gmail.com>
2018-06-07 13:42:57 -0600

    Committing with new database

--- database-connection.js
+++ database-connection.js
@@ -1,7 +1,11 @@
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./server/knexfile')
+/*const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')
 const environmentConfig = config[environment]
 const knex = require('knex')
 const connection = knex(environmentConfig)
 
 module.exports = connection
+*/
+
+const CONFIG = require("./knexfile")[process.env.NODE_ENV || "development"];
+module.exports = require("knex")(CONFIG);
